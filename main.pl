5dd72023ec5d06a60fedcc90b50bebb0fa1089fa
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-04-15 10:16:45 -0400

    Primer commit

---
+++ main.pl
@@ -0,0 +1,62 @@
+
+%generate_pos(BoardHeigth,
+%	     BoardWidth,
+%	     Dirty,
+%	     Obstacles,
+%	     Childs,
+%	     Corral,
+%	     Robots,
+%	     Pos):-
+%	A is random(5),
+%	B is random(20),
+%	append([A],[B],Pos),
+%	write(Pos),
+%	not(member(Pos,Dirty)),
+%	not(member(Pos,Obstacles)),
+%	not(member(Pos,Childs)),
+%	not(member(Pos,Corral)),
+%	not(member(Pos,Robots)),!.
+
+generate_env(BoardHeigth,
+			BoardWidth,
+			Agent,
+			DirtinessCount,
+			ObstacleCount,
+			ChildsCount,
+			Dirty,
+			Obstacles,
+			Childs,
+			Corral,
+			Robots):-
+				X is DirtinessCount-1,
+				write(X).
+
+
+main:-
+	Agent = 5,
+	BoardHeigth = 51,
+	BoardWidth = 50,
+	Time = 0,
+	TimeChange = 5,
+	DirtinessPercent = 17,
+	DirtinessCount is round((DirtinessPercent/100)*BoardHeigth*BoardWidth),
+	ObstaclePercent = 10,
+	ObstacleCount is round((ObstaclePercent/100)*BoardHeigth*BoardWidth),
+	ChildsCount = 10,
+	Dirty = [],
+	Obstacles = [],
+	Childs = [],
+	Corral = [],
+	Robots = [],
+	generate_env(BoardHeigth,
+			BoardWidth,
+			Agent,
+			DirtinessCount,
+			ObstacleCount,
+			ChildsCount,
+			Dirty,
+			Obstacles,
+			Childs,
+			Corral,
+			Robots).
+	
