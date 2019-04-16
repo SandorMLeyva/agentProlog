61733ccc96f2cb2179ced21620a3a82d6c107c73
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-04-15 20:10:09 -0400

    Genera obstaculos

--- main.pl
+++ main.pl
@@ -1,41 +1,33 @@
+generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Robots,Pos):-
+	A is random(BoardHeigth),
+	B is random(BoardWidth),
+	append([A],[B],Pos),
+	not(member(Pos,Dirty)),
+	not(member(Pos,Obstacles)),
+	not(member(Pos,Childs)),
+	not(member(Pos,Corral)),
+	not(member(Pos,Robots)),!.
 
-%generate_pos(BoardHeigth,
-%	     BoardWidth,
-%	     Dirty,
-%	     Obstacles,
-%	     Childs,
-%	     Corral,
-%	     Robots,
-%	     Pos):-
-%	A is random(5),
-%	B is random(20),
-%	append([A],[B],Pos),
-%	write(Pos),
-%	not(member(Pos,Dirty)),
-%	not(member(Pos,Obstacles)),
-%	not(member(Pos,Childs)),
-%	not(member(Pos,Corral)),
-%	not(member(Pos,Robots)),!.
+generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Robots,Pos):-
+		generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Robots,Pos).
 
-generate_env(BoardHeigth,
-			BoardWidth,
-			Agent,
-			DirtinessCount,
-			ObstacleCount,
-			ChildsCount,
-			Dirty,
-			Obstacles,
-			Childs,
-			Corral,
-			Robots):-
-				X is DirtinessCount-1,
-				write(X).
+generate_env(BoardHeigth,BoardWidth,Agent,DirtinessCount,ObstacleCount,ChildsCount,Dirty,Obstacles,Childs,Corral,Robots,ResultObstacles):-
+	ObstacleCount > 0,
+	X is ObstacleCount-1,
+	generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Robots,Pos),
+	Result = [Pos|Obstacles],
+	generate_env(BoardHeigth,BoardWidth,Agent,DirtinessCount,X,ChildsCount,Dirty,Result,Childs,Corral,Robots,Result2),
+	append([],Result2,ResultObstacles),!.
+
+generate_env(BoardHeigth,BoardWidth,Agent,DirtinessCount,0,ChildsCount,Dirty,Obstacles,Childs,Corral,Robots,ResultObstacles):- 
+	ResultObstacles = Obstacles.
+			
 
 
 main:-
 	Agent = 5,
-	BoardHeigth = 51,
-	BoardWidth = 50,
+	BoardHeigth = 50,
+	BoardWidth = 545,
 	Time = 0,
 	TimeChange = 5,
 	DirtinessPercent = 17,
@@ -48,15 +40,7 @@ main:-
 	Childs = [],
 	Corral = [],
 	Robots = [],
-	generate_env(BoardHeigth,
-			BoardWidth,
-			Agent,
-			DirtinessCount,
-			ObstacleCount,
-			ChildsCount,
-			Dirty,
-			Obstacles,
-			Childs,
-			Corral,
-			Robots).
+	generate_env(BoardHeigth,BoardWidth,Agent,DirtinessCount,ObstacleCount,ChildsCount,Dirty,Obstacles,Childs,Corral,Robots,ObstaclesEnv),
+	write(ObstaclesEnv).
+	%write(ObstaclesEnv).
 	
