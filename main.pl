0926de9a3205e4627e93e1f544e0b34ea808f895
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-04-21 13:48:44 -0400

    corrals done

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
-generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos):-
-	A is random(BoardHeigth),
+generate_pos(BoardHeight,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos):-
+	A is random(BoardHeight),
 	B is random(BoardWidth),
 	append([A],[B],Pos),
 	not(member(Pos,Dirty)),
@@ -7,74 +7,98 @@ generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos):-
 	not(member(Pos,Childs)),
 	not(member(Pos,Corral)),!.
 
-generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos):-
-		generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos).
+generate_pos(BoardHeight,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos):-
+		generate_pos(BoardHeight,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos).
 
-generate_obstacle(BoardHeigth,BoardWidth,ObstacleCount,Dirty,Obstacles,Childs,Corral,ResultObstacles):-
+generate_obstacle(BoardHeight,BoardWidth,ObstacleCount,Dirty,Obstacles,Childs,Corral,ResultObstacles):-
 	ObstacleCount > 0,
 	X is ObstacleCount-1,
-	generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos),
+	generate_pos(BoardHeight,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos),
 	Result = [Pos|Obstacles],
-	generate_obstacle(BoardHeigth,BoardWidth,X,Dirty,Result,Childs,Corral,Result2),
+	generate_obstacle(BoardHeight,BoardWidth,X,Dirty,Result,Childs,Corral,Result2),
 	append([],Result2,ResultObstacles),!.
 
-generate_obstacle(BoardHeigth,BoardWidth,0,Dirty,Obstacles,Childs,Corral,ResultObstacles):- 
+generate_obstacle(BoardHeight,BoardWidth,0,Dirty,Obstacles,Childs,Corral,ResultObstacles):- 
 	ResultObstacles = Obstacles.
 			
-generate_dirty(BoardHeigth,BoardWidth,DirtinessCount,Dirty,Obstacles,Childs,Corral,ResultDirtiness):-
+generate_dirty(BoardHeight,BoardWidth,DirtinessCount,Dirty,Obstacles,Childs,Corral,ResultDirtiness):-
 	DirtinessCount > 0,
 	X is DirtinessCount-1,
-	generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos),
+	generate_pos(BoardHeight,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos),
 	Result = [Pos|Dirty],
-	generate_dirty(BoardHeigth,BoardWidth,X,Result,Obstacles,Childs,Corral,Result2),
+	generate_dirty(BoardHeight,BoardWidth,X,Result,Obstacles,Childs,Corral,Result2),
 	append([],Result2,ResultDirtiness),!.
 
-generate_dirty(BoardHeigth,BoardWidth,0,Dirty,Obstacles,Childs,Corral,ResultDirtiness):- 
+generate_dirty(BoardHeight,BoardWidth,0,Dirty,Obstacles,Childs,Corral,ResultDirtiness):- 
 	ResultDirtiness = Dirty.
 
-generate_childs(BoardHeigth,BoardWidth,ChildsCount,Dirty,Obstacles,Childs,Corral,ResultChilds):-
+generate_childs(BoardHeight,BoardWidth,ChildsCount,Dirty,Obstacles,Childs,Corral,ResultChilds):-
 	ChildsCount > 0,
 	X is ChildsCount-1,
-	generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos),
+	generate_pos(BoardHeight,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos),
 	Result = [Pos|Childs],
-	generate_childs(BoardHeigth,BoardWidth,X,Dirty,Obstacles,Result,Corral,Result2),
+	generate_childs(BoardHeight,BoardWidth,X,Dirty,Obstacles,Result,Corral,Result2),
 	append([],Result2,ResultChilds),!.
 
-generate_childs(BoardHeigth,BoardWidth,0,Dirty,Obstacles,Childs,Corral,ResultChilds):- 
+generate_childs(BoardHeight,BoardWidth,0,Dirty,Obstacles,Childs,Corral,ResultChilds):- 
 	ResultChilds = Childs.
 
+sample(L, R) :- length(L, Len), random(0, Len, Random), nth0(Random, L, R).
 
-generate_childs(BoardHeigth,BoardWidth,ChildsCount,Dirty,Obstacles,Childs,Corral,ResultChilds):-
-	ChildsCount > 0,
-	X is ChildsCount-1,
-	generate_pos(BoardHeigth,BoardWidth,Dirty,Obstacles,Childs,Corral,Pos),
-	Result = [Pos|Childs],
-	generate_childs(BoardHeigth,BoardWidth,X,Dirty,Obstacles,Result,Corral,Result2),
-	append([],Result2,ResultChilds),!.
+generate_corral(BoardHeight, BoardWidth, CorralCount, Corrales, CorralResult) :-
+	CorralCount > 0,
+	C is CorralCount-1,
+	random(0, 4, R),
+    sample(Corrales,[X,Y]),
+    ((R =:= 0, X1 is X + 1,
+        append([X1],[Y],Position),
+        X1 > -1, X1 < BoardHeight, Y > -1, Y < BoardWidth);
+     (R =:= 1, X1 is X - 1,
+        append([X1],[Y],Position),
+        X1 > -1, X1 < BoardHeight, Y > -1, Y < BoardWidth);
+     (R =:= 2, Y1 is Y + 1,
+        append([X],[Y1],Position),
+        X > -1, X < BoardHeight, Y1 > -1, Y1 < BoardWidth);
+     (R =:= 3, Y1 is Y - 1,
+        append([X],[Y1],Position),
+        X > -1, X < BoardHeight, Y1 > -1, Y1 < BoardWidth)),
 
-generate_childs(BoardHeigth,BoardWidth,0,Dirty,Obstacles,Childs,Corral,ResultChilds):- 
-	ResultChilds = Childs.
+	not(member(Position, Corrales)),
+	Result = [Position|Corrales],
+			
+	generate_corral(BoardHeight, BoardWidth, C, Result, Result2),
+	append([],Result2,CorralResult),!.
 
-%generate_corral(BoardHeigth,BoardWidth,CorralCount,Dirty,Obstacles,Childs,Corral,ResultDirtiness)
+
+generate_corral(BoardHeight, BoardWidth,0, Corrales, CorralResult) :-
+	CorralResult = Corrales.
+
+generate_corral(BoardHeight, BoardWidth,CorralCount, Corrales, CorralResult) :-
+	generate_corral(BoardHeight, BoardWidth,CorralCount, Corrales, CorralResult).
+	
+
+
+
 
 main:-
-	BoardHeigth = 50,
-	BoardWidth = 10,
+	BoardHeight = 50,
+	BoardWidth = 50,
 	Time = 0,
 	TimeChange = 5,
+	ChildsCount = 10,
 	DirtinessPercent = 17,
 	ObstaclePercent = 10,
-	DirtinessCount is round((DirtinessPercent/100)*BoardHeigth*BoardWidth),
-	ObstacleCount is round((ObstaclePercent/100)*BoardHeigth*BoardWidth),
-	ChildsCount = 10,
+	DirtinessCount is round((DirtinessPercent/100)*BoardHeight*BoardWidth),
+	ObstacleCount is round((ObstaclePercent/100)*BoardHeight*BoardWidth),
 	Dirty = [],
 	Obstacles = [],
 	Childs = [],
-	Corral = [],
-	generate_obstacle(BoardHeigth,BoardWidth,ObstacleCount,Dirty,Obstacles,Childs,Corral,ObstaclesEnv),
-	generate_dirty(BoardHeigth,BoardWidth,DirtinessCount,Dirty,ObstaclesEnv,Childs,Corral,ResultDirtiness),
-	generate_childs(BoardHeigth,BoardWidth,ChildsCount,ResultDirtiness,ObstaclesEnv,Childs,Corral,ResultChilds),
-	generate_pos(BoardHeigth,BoardWidth,ResultDirtiness,ObstaclesEnv,ResultChilds,Corral,Robot).
-
+	generate_pos(BoardHeight,BoardWidth,[],[],[],[],Corral),
+	generate_corral(BoardHeight, BoardWidth, ChildsCount, [Corral], Result),
+	generate_obstacle(BoardHeight,BoardWidth,ObstacleCount,Dirty,Obstacles,Childs,Corral,ObstaclesEnv),
+	generate_dirty(BoardHeight,BoardWidth,DirtinessCount,Dirty,ObstaclesEnv,Childs,Corral,ResultDirtiness),
+	generate_childs(BoardHeight,BoardWidth,ChildsCount,ResultDirtiness,ObstaclesEnv,Childs,Corral,ResultChilds),
+	%generate_pos(BoardHeight,BoardWidth,ResultDirtiness,ObstaclesEnv,ResultChilds,Corral,Robot).
+	%Falta calcular los corrales
 	%write(ObstaclesEnv).
 	
