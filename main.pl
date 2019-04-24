131a8d6261786946002600e786ba07649a7028de
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-04-24 12:15:50 -0400

    bfs done

--- main.pl
+++ main.pl
@@ -204,6 +204,71 @@ move_obstacle(BoardHeight, BoardWidth, Dirty,Obstacles,Childs,Corral, [I,J], [X,
 move_obstacle(BoardHeight, BoardWidth, Dirty,Obstacles,Childs,Corral, [I,J], [X,Y], Obstacles).
 
 
+
+
+
+% Helpers Predicates
+adjacent(X, Y, X1, Y):- X1 is X+1.
+adjacent(X, Y, X1, Y):- X1 is X-1.
+adjacent(X, Y, X, Y1):- Y1 is Y+1.
+adjacent(X, Y, X, Y1):- Y1 is Y-1.
+adjacent(X, Y, X1, Y1):- X1 is X+1, Y1 is Y+1.
+adjacent(X, Y, X1, Y1):- X1 is X+1, Y1 is Y-1.
+
+
+is_dirt(X, Y, Dirtlist):- member((X, Y), Dirtlist).
+inside_enviroment(X, Y, N, M):-
+	X>=0,X<N, 
+	Y>=0,Y<M.
+
+% temporary almost dirty definition
+is_very_dirty(Dirts, _, _, N, M):- 
+	length(Dirts, Size), 
+	N * M * 60 / 100 =< Size + N + M.
+
+%=====================================================================+
+
+% BFS Helpers Predicates
+visited(Node, [LastPath|[]]):-member(Node, LastPath), !.
+visited(Node, [Path|Paths]):-
+	member(Node, Path);
+	visited(Node, Paths).
+
+expand([[[X, Y]|Path]|Paths], N, M, Obstacles, Extended):-
+    findall(
+        [[U,V], [X, Y]|Path],
+        (
+            adjacent(X, Y, U, V),	
+            inside_enviroment(U, V, N, M),
+            not(member([U, V], Obstacles)),
+        not(visited([U,V], [[[X, Y]|Path]|Paths]))
+    )
+, Extended).
+
+% BFS(PathsQueue, Rows, Cols, Obstacles, Goals, Solution)
+% returns the first shortest path to a goal from the list of goals
+bfs([[Node|Path]|_], _, _, _, GoalsList, Solution):-
+	member(Node, GoalsList),
+	reverse([Node|Path], Solution),	!.
+
+bfs(PathsQueue, N, M, Obstacles, GoalsList, Solution):-
+	PathsQueue = [_|Paths],
+	expand(PathsQueue, N, M, Obstacles, Extended),
+	append(Paths, Extended, Path1),
+	bfs(Path1, N, M, Obstacles, GoalsList, Solution).
+
+%=====================================================================+
+
+% Robot Agent:
+
+should_drop(Corrals, Position, [drop]):-member(Position, Corrals).
+should_drop(Corrals, Position, []):-not(member(Position, Corrals)).
+
+
+
+
+
+
 main:-
 	BoardHeight = 15,
 	BoardWidth = 15,
@@ -229,8 +294,14 @@ main:-
 	%writeln(Elem),
 	%write(DirtyResult),
 
-	move_obstacle(BoardHeight, BoardWidth, [],[[1,1],[2,2],[3,3],[4,1],[5,1],[6,1]],[],[], [1,1], [0,0], Result),
-	writeln(Result).
+
+	bfs([[[0, 1]]], BoardHeight, BoardWidth, [[1,1],[2,2],[3,3],[4,1],[5,1],[6,1]], [[5,5]], Path),
+	writeln(Path).
+
+
+
+	%move_obstacle(BoardHeight, BoardWidth, [],[[1,1],[2,2],[3,3],[4,1],[5,1],[6,1]],[],[], [1,1], [0,0], Result),
+	%writeln(Result).
 	
 
 	%generate_pos(BoardHeight,BoardWidth,ResultDirtiness,ObstaclesEnv,ResultChilds,Corral,Robot).
